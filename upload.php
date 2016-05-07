b7c38b8ec7421174fcb59a957cca1569eb477088
Etienne Ott <eti.ott@gmail.com>
2016-05-07 10:26:33 +0200

    Extend upload script.

--- upload.php
+++ upload.php
@@ -1,5 +1,9 @@
 <?php
+    ini_set("display_errors", true);
+    error_reporting(E_ALL);
+
     const MAX_FILE_UPLOAD_NR = 10;
+    const MAX_UPLOAD_SIZE_IN_MB = 5.0;
 
     $targetDir = "assets/uploads/";
     if (!is_dir($targetDir)) {
@@ -10,43 +14,48 @@
 
     // We hardcode a limit here in case the user tried to sneak in more
     // form elements than we can realistically handle
-    for ($i = 0; $i < MAX_FILE_UPLOAD_NR; $i++) {
-        if (!isset($_FILES["file_$i"])) {
+    for ($i = 1; $i <= MAX_FILE_UPLOAD_NR; $i++) {
+        $elementName = "file_" . $i;
+
+        if (!isset($_FILES[$elementName]["tmp_name"]) || empty($_FILES[$elementName]["tmp_name"])) {
             break;
         }
 
-        $errors["file_$i"] = array();
+        $errors[$elementName] = array();
 
         // Check if image file is an actual image or fake image
-        $check = getimagesize($_FILES["file_$i"]["tmp_name"]);
+        $check = getimagesize($_FILES[$elementName]["tmp_name"]);
         if ($check === false)  {
-            $errors["file_$i"][] = "File is not an image.";
-            continue;
-        }
-
-        $targetFile = $targetDir . basename($_FILES["file_$i"]["name"]);
-        // Check if file already exists
-        if (file_exists($targetFile)) {
-            $errors["file_$i"][] = "File already exists.";
+            $errors[$elementName][] = "File is not an image.";
             continue;
         }
 
         // Check file size
-        if ($_FILES["file_$i"]["size"] > 2000000) {
-            $errors["file_$i"][] = "File is too large (max 2MB).";
+        if ($_FILES[$elementName]["size"] > MAX_UPLOAD_SIZE_IN_MB * 1e6) {
+            $errors[$elementName][] = "File is too large (max " . MAX_UPLOAD_SIZE_IN_MB . "MB).";
             continue;
         }
 
         // Allow certain file formats
-        $imageFileType = pathinfo($targetFile, PATHINFO_EXTENSION);
-        if (!in_array($imageFileType, array("jpg", "jpeg", "png", "gif"))) {
-            $errors["file_$i"][] = "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+        $finfo = new finfo(FILEINFO_MIME_TYPE);
+        $ext = array_search(
+            $finfo->file($_FILES[$elementName]['tmp_name']),
+            array(
+                'jpg' => 'image/jpeg',
+                'png' => 'image/png',
+                'gif' => 'image/gif',
+            ),
+            true
+        );
+        if ($ext === false) {
+            $errors[$elementName][] = "Only JPG/JPEG, PNG & GIF files are allowed.";
             continue;
         }
 
         // Move uploaded file to target dir
-        if (!move_uploaded_file($_FILES["file_$i"]["tmp_name"], $targetFile)) {
-            $errors["file_$i"][] = "Sorry, there was an error uploading your file.";
+        $targetFile = $targetDir . preg_replace("#[^a-zA-Z0-9\._-]#", "", basename($_FILES[$elementName]["name"]));
+        if (!move_uploaded_file($_FILES[$elementName]["tmp_name"], $targetFile)) {
+            $errors[$elementName][] = "There was an error uploading your file.";
             continue;
         }
     }
@@ -63,15 +72,17 @@
     <body>
         <form action="upload.php" method="post" id="uploadForm" enctype="multipart/form-data">
             <?php for ($i = 1; $i <= MAX_FILE_UPLOAD_NR; $i++): ?>
-                <input type="file" name="file_$i" id="file_$i"/>
-                <?php if (!empty($errors["file_$i"])): ?>
+                <input type="file" name="file_<?php echo $i; ?>" id="file_<?php echo $i; ?>"/>
+                <?php if (!empty($errors["file_" . $i])): ?>
                     <ul class="errors">
-                        <?php foreach ($errors["file_$i"] as $error): ?>
+                        <?php foreach ($errors["file_" . $i] as $error): ?>
                             <li class="error"><?php echo $error; ?></li>
                         <?php endforeach; ?>
                     </ul>
                 <?php endif; ?>
             <?php endfor;?>
+
+            <input type="submit" name="submit" id="submit"/>
         </form>
     </body>
 </html>
